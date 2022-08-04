a4afb962f6600bcc27fbdf30ba7946aa0a956b80
Kangonga <guchu413@gmail.com>
2022-08-05 00:57:00 +0300

    Add index.js and styles file

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const sub = document.createElement("h2");
-sub.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(sub);
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+document.querySelector("body").appendChild(h2);
